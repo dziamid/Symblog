ffd569e80d9bb67b097b4a2f91d69bedd55430f1
Dziamid <dziamid@gmail.com>
2011-11-03 18:56:05 +0300

    added contact page

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -17,4 +17,9 @@ class PageController extends Controller
     {
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
+
+    public function contactAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig');
+    }
 }
