5dbbeef175c761cce29dbc873bfec3b42af1e74a
Dziamid <dziamid@gmail.com>
2011-11-03 19:34:00 +0300

    added Enquiry entity and form and fixed contact page to display and process form

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -3,7 +3,8 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Blogger\BlogBundle\Entity\Enquiry;
+use Blogger\BlogBundle\Form\EnquiryType;
 
 class PageController extends Controller
 {
@@ -20,6 +21,19 @@ class PageController extends Controller
 
     public function contactAction()
     {
-        return $this->render('BloggerBlogBundle:Page:contact.html.twig');
+        $enquiry = new Enquiry();
+        $form = $this->createForm(new EnquiryType(), $enquiry);
+        $request = $this->getRequest();
+
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+            if ($form->isValid()) {
+
+                return $this->redirect($this->generateUrl('BBBundle_contact'));
+            }
+        }
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Blogger\BlogBundle\Entity;
+
+class Enquiry
+{
+    protected $name;
+
+    protected $body;
+
+    protected $email;
+
+    protected $subject;
+
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+}
