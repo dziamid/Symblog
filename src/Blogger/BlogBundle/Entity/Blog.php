01329daf958d8bfda8fd434e2b83f98ca72e6722
Dziamid <dziamid@gmail.com>
2011-11-09 19:58:28 +0300

    blog entity

---
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Blogger\BlogBundle\Blog;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="blog")
+ */
+class Blog
+{
+    protected $title;
+
+    protected $author;
+
+    protected $image;
+
+    protected $tags;
+
+    protected $comments;
+
+    protected $created;
+
+    protected $updated;
+
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setComments($comments)
+    {
+        $this->comments = $comments;
+    }
+
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+    }
+
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+    }
+
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+}
