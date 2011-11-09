9fe6b83476534f0742ed5943c3285064b93eb4b0
Dziamid <dziamid@gmail.com>
2011-11-09 21:18:08 +0300

    added Blog entity

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -1,93 +1,167 @@
 <?php
 
-namespace Blogger\BlogBundle\Blog;
+namespace Blogger\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * Blogger\BlogBundle\Entity\Blog
+ *
  * @ORM\Table(name="blog")
+ * @ORM\Entity(repositoryClass="Blogger\BlogBundle\Entity\BlogRepository")
  */
 class Blog
 {
-    protected $title;
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
-    protected $author;
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
 
-    protected $image;
+    /**
+     * @var string $author
+     *
+     * @ORM\Column(name="author", type="string", length=100)
+     */
+    private $author;
 
-    protected $tags;
+    /**
+     * @var string $image
+     *
+     * @ORM\Column(name="image", type="string", length=20)
+     */
+    private $image;
 
-    protected $comments;
+    /**
+     * @var datetime $created
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
 
-    protected $created;
+    /**
+     * @var datetime $updated
+     *
+     * @ORM\Column(name="updated", type="datetime")
+     */
+    private $updated;
 
-    protected $updated;
 
-
-    public function setAuthor($author)
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
     {
-        $this->author = $author;
+        return $this->id;
     }
 
-    public function getAuthor()
-    {
-        return $this->author;
-    }
-
-    public function setComments($comments)
-    {
-        $this->comments = $comments;
-    }
-
-    public function getComments()
-    {
-        return $this->comments;
-    }
-
-    public function setCreated($created)
-    {
-        $this->created = $created;
-    }
-
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    public function setImage($image)
-    {
-        $this->image = $image;
-    }
-
-    public function getImage()
-    {
-        return $this->image;
-    }
-
-    public function setTags($tags)
-    {
-        $this->tags = $tags;
-    }
-
-    public function getTags()
-    {
-        return $this->tags;
-    }
-
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
     public function setTitle($title)
     {
         $this->title = $title;
     }
 
+    /**
+     * Get title
+     *
+     * @return string 
+     */
     public function getTitle()
     {
         return $this->title;
     }
 
+    /**
+     * Set author
+     *
+     * @param string $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string 
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set created
+     *
+     * @param datetime $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * Get created
+     *
+     * @return datetime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set updated
+     *
+     * @param datetime $updated
+     */
     public function setUpdated($updated)
     {
         $this->updated = $updated;
     }
 
+    /**
+     * Get updated
+     *
+     * @return datetime 
+     */
     public function getUpdated()
     {
         return $this->updated;
