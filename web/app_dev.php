261087435082f65c1fba4870f4976e3274241a81
Dziamid <dziamid@gmail.com>
2012-01-25 17:06:14 +0300

    fixed app_dev to ease debugging

--- web/app_dev.php
+++ web/app_dev.php
@@ -14,11 +14,24 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-require_once __DIR__.'/../app/bootstrap.php.cache';
-require_once __DIR__.'/../app/AppKernel.php';
+
+const CLASS_CACHING = true;
+
+if (false == CLASS_CACHING) {
+    require_once __DIR__.'/../app/bootstrap.php.cache';
+    $kernel->loadClassCache();
+} else {
+    require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+    require_once __DIR__.'/../app/autoload.php';
+}
 
 use Symfony\Component\HttpFoundation\Request;
 
+require_once __DIR__.'/../app/AppKernel.php';
+
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
+
+
 $kernel->handle(Request::createFromGlobals())->send();
+
+
